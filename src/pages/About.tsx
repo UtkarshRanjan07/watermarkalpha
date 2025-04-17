c6cc9ac97b4e8d4cb50b541d49a9a82417f1ba80
Utkarsh Ranjan <utkarshranjan@Utkarshs-MacBook-Air.local>
2025-04-17 14:49:17 +0530

    Hide vision image on mobile view

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -89,8 +89,9 @@ const About = () => {
               <img
                 src="https://images.unsplash.com/photo-1605792657660-596af9009e82?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2204&q=80"
                 alt="Digital Asset Management"
-                className="relative rounded-xl object-cover w-full h-[500px]"
+                className="relative rounded-xl object-cover w-full h-[500px] hidden md:block"
               />
+
             </div>
           </div>
         </div>
